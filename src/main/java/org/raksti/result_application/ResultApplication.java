5bbdbfdc41be94e46c981201ada9e2f9a6973a51
artjoms-melihovs <artjoms.melihovs@posti.com>
2023-02-10 18:04:15 +0200

    Whitelabel disabled, custom error page added. Unused dependencies removed

--- src/main/java/org/raksti/result_application/ResultApplication.java
+++ src/main/java/org/raksti/result_application/ResultApplication.java
@@ -2,9 +2,8 @@ package org.raksti.result_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 public class ResultApplication {
 
     public static void main(String[] args) {
